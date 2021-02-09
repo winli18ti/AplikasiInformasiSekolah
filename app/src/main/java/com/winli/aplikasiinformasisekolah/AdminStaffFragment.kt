614f21a02217f379c7472feffa75efb934c4e04e
widya <widya18ti@mahasiswa.pcr.ac.id>
2021-02-10 05:37:55 +0700

    merge

--- app/src/main/java/com/winli/aplikasiinformasisekolah/AdminStaffFragment.kt
+++ app/src/main/java/com/winli/aplikasiinformasisekolah/AdminStaffFragment.kt
@@ -13,11 +13,32 @@ class AdminStaffFragment : Fragment() {
     private lateinit var listData: ListView
     private lateinit var ref: DatabaseReference
     private lateinit var staffList: MutableList<Staff>
+    private lateinit var sekolahList: MutableList<Sekolah>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val refSekolah = FirebaseDatabase.getInstance().getReference("sekolah")
+        sekolahList = mutableListOf()
+        refSekolah.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()) {
+                    sekolahList.clear()
+                    for (s in snapshot.children) {
+                        val sekolah = s.getValue(Sekolah::class.java)
+                        if (sekolah != null) {
+                            sekolahList.add(sekolah)
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
         val view = inflater.inflate(R.layout.fragment_admin_staff, container, false)
         ref = FirebaseDatabase.getInstance().getReference("staff")
         listData = view.findViewById(R.id.list_staff)
@@ -30,6 +51,12 @@ class AdminStaffFragment : Fragment() {
                     for (s in snapshot.children) {
                         val staff = s.getValue(Staff::class.java)
                         if (staff != null) {
+                            for (sekolah in sekolahList) {
+                                if (sekolah.id_sekolah.equals(staff.id_sekolah)) {
+                                    staff.id_sekolah = sekolah.nama_sekolah
+                                }
+                            }
+
                             staffList.add(staff)
                         }
                     }
